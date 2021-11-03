72d2fc8f286275cda38d72bff07bf82705ee902c
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-11-03 11:39:53 +0700

    add user model

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import db from "./config/database.js";
+import Users from "./models/UserModel.js";
 
 const app = express();
 
 try {
     await db.authenticate();
     console.log('Database Connected...');
+    await Users.sync();
 } catch (error) {
     console.error(error);
 }
